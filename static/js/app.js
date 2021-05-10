49066f146a27e6091a6b0cd86e928429e1bf4294
fkimani <mainafk@gmail.com>
2021-05-09 21:37:09 -0400

    script populates data to html

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select table body
+var tbody = d3.select("table");
+// console.log(tableData)
+
+// loop through data and print each object
+tableData.forEach(function(ufoData){
+    // add table row
+    var row = tbody.append("tr");
+    
+    Object.entries(ufoData).forEach(function([key,value]){
+        console.log(key,value);
+
+        // add table data
+        var cell = tbody.append("td");
+
+        //add data values to the table rows
+        cell.text(value);
+        // console.log(key,value);
+    });
+    
+});
+
+
